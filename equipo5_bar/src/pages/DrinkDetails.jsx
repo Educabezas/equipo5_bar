394c639ef3d4d32253a73fdbc37462b654e07506
Marcela Wojtasik <marcelawojtasik@gmail.com>
2024-05-08 19:52:15 -0300

    Card y Grid

---
+++ equipo5_bar/src/pages/DrinkDetails.jsx
@@ -0,0 +1,35 @@
+import { get } from "../utils/conexionDetailsAPI"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+//ESTO TRAE EL ARRAY PERO NO MUESTRA NADAAAAAAAAAAAAA
+export const DrinkDetails = () => {
+    
+    const [drinks, setDrinks] = useState (null)
+    const {tragosId} = useParams()
+        
+    useEffect(()=>{
+        get(`${tragosId}`).then((data)=>{
+         
+            console.log(data.drinks)
+            setDrinks(data.drinks)
+        })
+    }), [tragosId]
+
+    if(!drinks){
+        return (
+            "Algo raro pasa"
+        )
+    }
+
+    return(
+        <div className="detail">
+        <img className="imgDrink" src={strDrinkThumb} alt="No me trae la img" />
+            <div className="drinkDetail">
+                <p className="drink">
+                    Nombre e Info del trago  
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ equipo5_bar/src/utils/conexionAPI.js
@@ -0,0 +1,6 @@
+const API = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+
+export const get = () => {
+    return fetch (API).then((response)=>response.json())
+}
+
